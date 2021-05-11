6cb0821fa72341b4f0ea1afb45304ea9b01bc987
XandDevWeb <69404289+XandDevWeb@users.noreply.github.com>
2021-05-11 16:27:10 -0300

    add main on view/questao

---
+++ app/view/questao/main.php
@@ -0,0 +1,99 @@
+<div class="path">
+            <a href="#">Matéria</a>
+            <span>></span>
+            <a href="#">Assunto</a>
+        </div>
+        <section>
+            <ul>
+                <li>
+                    <span>Questão 1 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 2 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 3 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span style="color: green;">&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 4 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 5 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 6 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 7 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 8 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 9 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 10 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 11 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 12 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 13 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 14 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+                <li>
+                    <span>Questão 15 - Começo da questão...</span>
+                    <div class="question-icons">
+                        <span>&#10003;</span>
+                    </div>
+                </li>
+            </ul>
+        </section>
