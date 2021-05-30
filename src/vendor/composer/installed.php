e6da4f05c84807e73722ce067d27f922d2cb0abc
XandDevWeb <69404289+XandDevWeb@users.noreply.github.com>
2021-05-30 14:43:21 -0300

    updated composer

--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -1,24 +1,24 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => 'b8fe915288d2671e3a09c64a5d4c0ce984d26c28',
     'name' => 'bancodequestoes/trabalho',
   ),
   'versions' => 
   array (
     'bancodequestoes/trabalho' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => 'b8fe915288d2671e3a09c64a5d4c0ce984d26c28',
     ),
   ),
 );
